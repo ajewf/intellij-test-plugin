e165f812cbf006bc0379402c51c59e99d621a165
GitHub Action <action@github.com>
2023-03-22 17:52:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/ajewf/intellijtestplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ajewf/intellijtestplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ajewf.intellijtestplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ajewf.intellijtestplugin.MyBundle
+import com.github.ajewf.intellijtestplugin.services.MyProjectService
 import javax.swing.JButton
 
 
